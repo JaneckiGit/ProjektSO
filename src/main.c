23dc03347c2d3b371f6eeaa1d6fdfe3b34dbf785
Mateusz Janecki <100022533+JaneckiGit@users.noreply.github.com>
2025-12-28 14:29:31 +0000

    Implement main simulation entry point with argument parsing and validation

---
+++ src/main.c
@@ -0,0 +1,38 @@
+// main.c - Punkt startowy symulacji
+#include "dyspozytor.h"
+
+int main(int argc, char *argv[]) {
+    int N = 3;     // Liczba autobusow
+    int P = 10;    // Pojemnosc
+    int R = 3;     // Rowery
+    int T = 5000;  // Postoj [ms]
+
+    // Parsowanie argumentow
+    if (argc > 1) N = atoi(argv[1]);
+    if (argc > 2) P = atoi(argv[2]);
+    if (argc > 3) R = atoi(argv[3]);
+    if (argc > 4) T = atoi(argv[4]);
+
+    // Walidacja
+    if (N <= 0 || N > MAX_BUSES) {
+        fprintf(stderr, "Blad: N musi byc 1-%d\n", MAX_BUSES);
+        return 1;
+    }
+    if (P <= 0 || P > MAX_CAPACITY) {
+        fprintf(stderr, "Blad: P musi byc 1-%d\n", MAX_CAPACITY);
+        return 1;
+    }
+    if (R < 0 || R > P) {
+        fprintf(stderr, "Blad: R musi byc 0-%d\n", P);
+        return 1;
+    }
+    if (T <= 0) {
+        fprintf(stderr, "Blad: T musi byc > 0\n");
+        return 1;
+    }
+
+    // Start symulacji
+    proces_dyspozytor(N, P, R, T);
+
+    return 0;
+}
